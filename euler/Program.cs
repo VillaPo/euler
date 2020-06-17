7e5ec8248b7a982c8d0bd0de0a9ae7c1403f2e0d
artiq <art.iq@hotmail.com>
2020-06-17 18:31:36 +0300

    Написан первый метод

--- euler/Program.cs
+++ euler/Program.cs
@@ -1,9 +1,20 @@
 ﻿using System;
 
 namespace euler
-{
+{    
     class Program
     {
+        /// <summary>
+        /// Определяет, делится ли одно число на другое без остатка.
+        /// </summary>
+        /// <param name="a">делитель</param>
+        /// <param name="b">делимое</param>
+        /// <returns>true - если b делится на a без остатка, false - если нет</returns>
+        static bool Aliquant(int a, int b)
+        {
+            return (b % a == 0);
+        }
+
         static void Main(string[] args)
         {
             
